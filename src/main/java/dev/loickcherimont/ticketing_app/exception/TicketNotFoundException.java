8472c14287f12afc2f98074b8bc446b128b8b27e
Loick Cherimont <loickcherimont@gmail.com>
2025-08-20 00:46:59 +0200

    feat: add TicketNotFoundException for handling missing tickets
    - Created TicketNotFoundException class to throw a 404 error when a ticket is not found.
    - Utilized Spring's @ResponseStatus annotation to set the HTTP status code.

---
+++ src/main/java/dev/loickcherimont/ticketing_app/exception/TicketNotFoundException.java
@@ -0,0 +1,13 @@
+package dev.loickcherimont.ticketing_app.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class TicketNotFoundException extends RuntimeException {
+
+    public TicketNotFoundException(String message) {
+        super(message);
+    }
+
+}
