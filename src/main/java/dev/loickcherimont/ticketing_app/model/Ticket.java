cc1623ee32b8e190f993221932c6dacd23e8d887
Loick Cherimont <loickcherimont@gmail.com>
2025-08-20 00:46:38 +0200

    feat: add Ticket and TicketStatus models
    - Introduced Ticket class with fields for id, title, description, status, and solution.
    - Created TicketStatus enum to represent the various states of a ticket (OPEN, IN_PROGRESS, CLOSED).

---
+++ src/main/java/dev/loickcherimont/ticketing_app/model/Ticket.java
@@ -0,0 +1,37 @@
+package dev.loickcherimont.ticketing_app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TicketStatus status;
+
+    @Column(columnDefinition = "TEXT")
+    private String solution; //  Null until the ticket is closed
+}
---
+++ src/main/java/dev/loickcherimont/ticketing_app/model/TicketStatus.java
@@ -0,0 +1,7 @@
+package dev.loickcherimont.ticketing_app.model;
+
+public enum TicketStatus {
+    OPEN,
+    IN_PROGRESS,
+    CLOSED
+}
