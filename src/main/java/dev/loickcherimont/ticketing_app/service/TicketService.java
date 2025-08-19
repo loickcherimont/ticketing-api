61f689b05d102b438e2a2d1032301c380ef02c4e
Loick Cherimont <loickcherimont@gmail.com>
2025-08-20 00:47:12 +0200

    feat: implement TicketService and TicketServiceImpl for ticket management
    - Created TicketService interface defining methods for ticket operations.
    - Implemented TicketServiceImpl class with methods to retrieve, create, solve, and update ticket statuses.
    - Integrated TicketRepository for database interactions.

---
+++ src/main/java/dev/loickcherimont/ticketing_app/service/TicketService.java
@@ -0,0 +1,14 @@
+package dev.loickcherimont.ticketing_app.service;
+
+import java.util.List;
+
+import dev.loickcherimont.ticketing_app.model.Ticket;
+
+public interface TicketService {
+    List<Ticket> getAllTickets();
+    Ticket getTicketById(Long id);
+    Ticket createTicket(Ticket ticket);
+    // Ticket updateTicket(Long id, Ticket ticket);
+    Ticket solveTicket(Long id, String solution);
+    Ticket setTicketInProgress(Long id);
+}
