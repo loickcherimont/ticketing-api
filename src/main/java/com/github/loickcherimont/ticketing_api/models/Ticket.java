a35150f0cc26b446fb03a9f3689c225ccb2ccb1c
loickcherimont <loickcherimont@gmail.com>
2026-05-27 20:41:15 +0200

    feat: add `unique` constraint on `title` to avoid duplication

--- src/main/java/com/github/loickcherimont/ticketing_api/models/Ticket.java
+++ src/main/java/com/github/loickcherimont/ticketing_api/models/Ticket.java
@@ -26,7 +26,7 @@ public class Ticket {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 100)
+    @Column(unique = true, nullable = false, length = 100)
     private String title;
 
     @Column(nullable = false, columnDefinition = "TEXT")
