b14e7a5b12f117058b706b4c596c2b4e0923f908
loickcherimont <loickcherimont@gmail.com>
2026-05-28 21:07:48 +0200

    feat: create the class to manage users fetching from db

---
+++ src/main/java/com/github/loickcherimont/ticketing_api/services/impl/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.github.loickcherimont.ticketing_api.services.impl;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Email non trouvé pour: " + username));
+
+        return new User(user.getUsername(), user.getPassword(), user.getAuthorities());
+    }
+
+
+}
