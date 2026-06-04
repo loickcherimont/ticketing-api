77d269630cb15f050fc41494e90dfd406e553f17
loickcherimont <loickcherimont@gmail.com>
2026-06-04 13:01:54 +0200

    fix: update response handling in controllers and enhance error responses in GlobalExceptionHandler

--- src/main/java/com/github/loickcherimont/ticketing_api/controllers/AuthController.java
+++ src/main/java/com/github/loickcherimont/ticketing_api/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package com.github.loickcherimont.ticketing_api.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,19 +12,16 @@ import com.github.loickcherimont.ticketing_api.dto.SigninResponseDto;
 import com.github.loickcherimont.ticketing_api.services.AuthService;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-@Slf4j
 public class AuthController {
 
     private final AuthService authService;
 
     @PostMapping("/signin")
     public ResponseEntity<SigninResponseDto> signin(@RequestBody SigninRequestDto signinRequestDto) {
-        log.info("\nController DTO: " + signinRequestDto.email());
-        return ResponseEntity.ok(authService.signin(signinRequestDto));
+        return ResponseEntity.status(HttpStatus.OK).body(authService.signin(signinRequestDto));
     }
 }
--- src/main/java/com/github/loickcherimont/ticketing_api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/github/loickcherimont/ticketing_api/exceptions/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.github.loickcherimont.ticketing_api.exceptions;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,6 +23,11 @@ public class GlobalExceptionHandler {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
 
-        return ResponseEntity.badRequest().body(errors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(TicketNotFoundException.class)
+    public ResponseEntity<String> handleTicketNotFound(TicketNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 }
